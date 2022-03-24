b203cf75a4590cc6b9b6f453a227d173eba39386
freQuensy <mamaetevalex@gmail.com>
2022-03-24 13:55:20 +0300

    Пересылка сообщений

---
+++ config.py
@@ -0,0 +1,2 @@
+api_id = 1308644
+api_hash = 'df0215899cd03b8c63cd70b7ed01b3ef'
--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-from telethon import TelegramClient, sync, events
+from telethon import TelegramClient, events
+from config import api_id, api_hash
+from MemList import MemoryList
 
-INPUT_CHANNEL = 'input_channel_username'
-OUTPUT_CHANNEL = 'output_channel_username'
-TAGS = ['#TAG1', '#TAG2']
-
-# 1. Заходим на сайт https://my.telegram.org/apps
-# 2. Заполняем поля App title и Short name, нажимаем «Create application» и запоминаем две переменные: api_id и api_hash.
+INPUT_CHANNELS = MemoryList(name="INPUT_CHANNELS")
+OUTPUT_CHANNEL = 'resend_channel'
 
 
-api_id = 1234567
-api_hash = 'apihash'
+client = TelegramClient('resend_bot', api_id, api_hash)
 
 
-client = TelegramClient('session_name', api_id, api_hash)
-
-
-
-
-@client.on(events.NewMessage(chats=(INPUT_CHANNEL)))
+@client.on(events.NewMessage(chats=INPUT_CHANNELS))
 async def normal_handler(event):
-    for tag in TAGS:
-
-        if tag in str(event.message):
-
-            await client.send_message(OUTPUT_CHANNEL, event.message)
+    if event.message.text.find("/add") != -1:
+        INPUT_CHANNELS.append(event.message.text.removeprefix("/add").strip())
+        return None
+    await client.send_message(OUTPUT_CHANNEL, event.message)
 
 client.start()
 client.run_until_disconnected()
